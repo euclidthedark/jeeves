ac1db72cf3c7a1fef180f4adf4066c6b08fbef08
euclidthedark <euclidthedark@gmail.com>
2025-02-17 16:27:21 -0500

    build out the first property on the http struct, which maps to an http header in ./http/lib.rs, and write tests

--- http/lib.rs
+++ http/lib.rs
@@ -1,9 +1,11 @@
+// TODO: make sure naming is correct.
 use std::io::{BufReader, BufRead};
 use std::net::TcpStream;
 
 struct Request {
     method: String,
     host: String,
+    version: String,
 }
 
 pub struct Http {
@@ -11,21 +13,24 @@ pub struct Http {
 }
 
 impl Http {
-    pub fn new(stream: &TcpStream) -> Self {
+    pub fn new(socket: &TcpStream) -> Self {
         // TODO: look into the memory implications of peek
-        let mut p = [0; 256];
-        println!("peeked: {}", stream.peek(&mut p).unwrap());
-        let data: Vec<String> = BufReader::new(stream)
+        let request: Vec<_> = BufReader::new(socket)
             .lines()
             .map(|line| line.unwrap())
-            .take_while(|line| !line.is_empty())
+            .take(255)
             .collect();
 
-        println!("{:?}", data);
+        println!("The request is:: {:?}", request);
+
+        let method: Vec<_> = request[0].split(" ").collect();
+
+
 
         let request = Request {
-            method: "GET".to_string(),
-            host: "localhost".to_string(),
+            method: method[0].to_string(),
+            host: method[1].to_string(),
+            version: method[2].to_string(),
         };
 
         Self {
@@ -43,11 +48,13 @@ mod tests {
     #[test]
     fn should_generate_http_state() -> Result<(), std::io::Error> {
         let mut stream = TcpStream::connect("localhost:3000").unwrap();
-        stream.write(b"GET localhost:8080 HTTP/1.0\r\n\r\n").unwrap();
+        stream.write(b"GET localhost:8080 HTTP/1.1\r\n\r\nHello").unwrap();
 
         let http = Http::new(&stream);
 
         assert_eq!(http.request.method, "GET".to_string());
+        assert_eq!(http.request.host, "localhost:8080".to_string());
+        assert_eq!(http.request.version, "HTTP/1.1".to_string());
         Ok(())
     }
 }
